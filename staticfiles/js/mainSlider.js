ef54735a1544d1c2c53aa84151be788ab6f14cd2
PrudkiyD <dmitro.prudkiy@gmail.com>
2026-05-20 11:24:24 +0000

    Remove staticfiles from tracking

--- staticfiles/js/mainSlider.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('.slider').slick({
-        speed: 700,
-        easing: 'ease',
-        autoplay: true,
-        autoplaySpeed: 1500,
-        variableWidth: false,
-    });
-});
