a15464732d8db72c91dc711f13d2f136c2f1ba93
PrudkiyD <dmitro.prudkiy@gmail.com>
2026-06-25 14:48:02 +0300

    catalog

--- catalog/views.py
+++ catalog/views.py
@@ -13,8 +13,6 @@ def catalog(request):
     pages = Page.objects.filter(published=True)
     title = "Всі категорії"
 
-    Product.objects.filter( manufacturer_id=19, external_category='get_kukhonni_kutochky_kompanit').update(published=False)
-
     return render(request, 'all-categorys.html', {
         'title': title,
         'categorys': categorys,
