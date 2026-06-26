64c8ba3d4e9aa9ec298961f87df97d6cd07b2902
PrudkiyD <dmitro.prudkiy@gmail.com>
2026-06-26 15:34:45 +0300

    pag

--- catalog/views.py
+++ catalog/views.py
@@ -105,7 +105,7 @@ def category(request, category, subcategory=None):
         products = products.order_by('-sale', 'price')
 
     # Пагінація
-    paginator = Paginator(products, 12)
+    paginator = Paginator(products, 25)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
